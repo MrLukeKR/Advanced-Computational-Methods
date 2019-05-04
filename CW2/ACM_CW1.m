f5a3e63ec15046ae7d4c11297e21ac2436caf5fb
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2019-05-04 14:01:55 +0000

    Initialise ACM Part 2

---
+++ CW2/ACM_CW1.m
@@ -0,0 +1,13 @@
+% -- PRELIMINARY SETUP --
+    clearvars; % Do not use clear all, it's inefficient -> clear variables
+    clc;
+
+    format long;
+    syms f
+% -----------------------
+
+% \/\/\/ Question (a) \/\/\/
+
+% \/\/\/ Question (b) \/\/\/
+
+% \/\/\/ Question (c) \/\/\/
